ed4243ce8c15d2633885bf79987caf054ea423ab
Morten Ydefeldt <morten@ydefeldt.com>
2015-02-13 01:01:09 +0100

    add UI and connection to firebase
    Firebase dosen’t quite work.. GO FIX. =)

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -30,7 +30,42 @@
   })
 
   .controller('HomeController', function(user) {
+
     this.user = user;
-  });
+
+    var self = this;
+
+    this.user.$loaded()
+    .then(function() {
+      self.user.snippets = self.user.snippets || [];
+    });
+    //this.snippets = [{content: 'This is some content', variables: [{tag: 'navn'},{tag: 'title'},{tag: 'test'}, {tag: 'mother'}]}, {content: 'This is some other content', variables: [{tag: 'navn'},{tag: 'title'},{tag: 'test'}]}]
+    this.currentSnippet = {
+      variables : []
+    };
+
+    this.addVariable = function () {
+      this.currentSnippet.variables.push({
+        tag: this.newPlaceholder
+      }
+    )
+    console.log("test")
+    this.newPlaceholder = "";
+  };
+
+  this.saveSnippet = function () {
+    console.log("MOTHFKDSJLKDSMSDKLFD");
+    this.user.snippets.push({
+      content: this.newContent
+    });
+
+    this.user.$save();
+  };
+
+
+
+});
+
+
 
 }());
